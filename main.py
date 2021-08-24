9c61130a484d0f0407eec14eaf6622a31e838f0e
Tomachi [ICHIGO] <lego@aol.jp>
2021-08-25 03:31:15 +0900

    feat: text検索機能の追加（設定ファイル仕様変更）

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ import json
 import requests
 
 
-def search(api, word: str):
+def search(api,
+           word: str):
     print("search({})".format(word))
 
     response = api.search_novel(word)
@@ -14,7 +15,86 @@ def search(api, word: str):
     return response["novels"]
 
 
-def sendMessage(channelId: str, message: str = "", embed: dict = None):
+def search_text(api,
+                word: str):
+    print("search_text({})".format(word))
+
+    response = api.search_novel(word, search_target="text")
+
+    return response["novels"]
+
+
+def process_search(api: AppPixivAPI,
+                   searchwords: dict,
+                   readed: list,
+                   config: dict,
+                   init: bool,
+                   key: str,
+                   func):
+    print("process_search({})".format(key))
+    for word in searchwords[key]:
+        results = func(api, word)
+        for result in results:
+            novelId = result["id"]
+
+            if novelId in readed:
+                continue
+
+            novelTitle = result["title"]
+            novelDate = result["create_date"].replace(
+                "T", " ").replace("+09:00", "")
+            novelTags = list(map(lambda x: x["name"], result["tags"]))
+            novelCaption = result["caption"]
+            novelCaption = novelCaption.replace("<br />", "\n")
+            novelCaption = re.sub(r"<strong>(.*)</strong>",
+                                  r"**\1**", novelCaption)
+            novelCaption = re.sub(
+                r"<a href=\"(.*?)\".*?>(.*?)</a>", r"[\2](\1)", novelCaption)
+            novelUsername = result["user"]["name"]
+
+            readed.append(novelId)
+
+            muted = False
+            for mutetag in searchwords["mutetags"]:
+                if mutetag in novelTags:
+                    muted = True
+                    break
+            if muted:
+                continue
+
+            embed = {
+                "title": "`{word}` -> `{title}`".format(word=word, title=novelTitle, username=novelUsername),
+                "type": "rich",
+                "url": "https://www.pixiv.net/novel/show.php?id={id}".format(id=novelId),
+                "description": novelCaption,
+                "fields": [
+                    {
+                        "name": "Tags",
+                        "value": "`{}`".format("`・`".join(novelTags))
+                    },
+                    {
+                        "name": "Author",
+                        "value": "`{}`".format(novelUsername)
+                    },
+                    {
+                        "name": "Date",
+                        "value": novelDate
+                    }
+                ],
+                "footer": {
+                    "text": "Search type: " + key
+                }
+            }
+
+            if not init:
+                sendMessage(config["discord_channel"], "", embed)
+
+    return readed
+
+
+def sendMessage(channelId: str,
+                message: str = "",
+                embed: dict = None):
     if not os.path.exists("config.json"):
         print("config.json not found.")
         exit(1)
@@ -76,69 +156,24 @@ def main():
         if searchwords["words"] != data["searchwords"]:
             init = True
             print("[INFO] Initialize mode (Changed searchwords)")
+
+        if searchwords["textwords"] != data["textwords"]:
+            init = True
+            print("[INFO] Initialize mode (Changed textwords)")
     else:
         print("[INFO] Initialize mode")
 
-    for word in searchwords["words"]:
-        results = search(api, word)
-        for result in results:
-            novelId = result["id"]
-
-            if novelId in readed:
-                continue
-
-            novelTitle = result["title"]
-            novelDate = result["create_date"].replace(
-                "T", " ").replace("+09:00", "")
-            novelTags = list(map(lambda x: x["name"], result["tags"]))
-            novelCaption = result["caption"]
-            novelCaption = novelCaption.replace("<br />", "\n")
-            novelCaption = re.sub(r"<strong>(.*)</strong>",
-                                  r"**\1**", novelCaption)
-            novelCaption = re.sub(
-                r"<a href=\"(.*?)\".*?>(.*?)</a>", r"[\2](\1)", novelCaption)
-            novelUsername = result["user"]["name"]
-
-            readed.append(novelId)
-
-            muted = False
-            for mutetag in searchwords["mutetags"]:
-                if mutetag in novelTags:
-                    muted = True
-                    break
-            if muted:
-                continue
-
-            embed = {
-                "title": "`{word}` -> `{title}`".format(word=word, title=novelTitle, username=novelUsername),
-                "type": "rich",
-                "url": "https://www.pixiv.net/novel/show.php?id={id}".format(id=novelId),
-                "description": novelCaption,
-                "fields": [
-                    {
-                        "name": "Tags",
-                        "value": "`{}`".format("`・`".join(novelTags))
-                    },
-                    {
-                        "name": "Author",
-                        "value": "`{}`".format(novelUsername)
-                    },
-                    {
-                        "name": "Date",
-                        "value": novelDate
-                    }
-                ]
-            }
-
-            if not init:
-                sendMessage(config["discord_channel"], "", embed)
+    # -------------
+    process_search(api, searchwords, readed, config, init, "words", search)
+    process_search(api, searchwords, readed, config, init, "textwords", search_text)
 
     print(readed)
 
     with open("data.json", "w", encoding="utf-8") as f:
         f.write(json.dumps({
             "readed": readed,
-            "searchwords": searchwords["words"]
+            "searchwords": searchwords["words"],
+            "textwords": searchwords["textwords"]
         }))
 
 
